ff3ba4884b066d0693dedd44995876709661eedc
Giovanni <giovidecaro@live.it>
2024-10-30 10:11:34 +0100

    footer removed

--- gym_app/src/app/layout.tsx
+++ gym_app/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,7 +32,6 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- gym_app/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="bg-black font-bold text-xl text-white uppercase text-center p-3">
-            <h1>All right reserved | Giovanni De Caro </h1>
-        </div>
-    )
-}
